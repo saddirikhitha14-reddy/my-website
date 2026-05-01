8b577ff1944d52cc87051a58e47198dcf12249d9
saddirikhitha14-reddy <saddirikhitha14@gmail.com>
2026-05-01 23:09:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function project1() {
+    alert("Hidden Camera Detector Project - detects hidden surveillance devices.");
+}
+
+function project2() {
+    alert("MIMO-OFDM Simulation Project - improves wireless communication performance.");
+}
